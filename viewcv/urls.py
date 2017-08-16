ff0ef33618ca7cd46d4a78a396a2e18447ec0045
Jarno Luoma-Nirva <jln@iki.fi>
2017-08-16 08:56:31 +0300

    Add profile page skeleton

--- viewcv/urls.py
+++ viewcv/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import HomeView
+from .views import HomeView, ProfileView
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^accounts/profile/$', ProfileView.as_view(), name='profile'),
 ]
--- viewcv/views.py
+++ viewcv/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'viewcv/home.html'
+
+
+class ProfileView(TemplateView):
+    template_name = 'viewcv/profile.html'
