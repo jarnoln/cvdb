28e8eced14f3d61b849adf3d9676e124bced9e10
Jarno Luoma-Nirva <jln@iki.fi>
2017-08-22 13:14:56 +0300

    Fix BDD test

--- features/environment.py
+++ features/environment.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from time import sleep
 
 
 def before_all(context):
@@ -6,7 +7,9 @@ def before_all(context):
 
 
 def after_all(context):
-    context.browser.quit()
+    sleep(1)
+    context.browser.close()  # Use close instead of quit (because quit gives error)
+    context.browser = None
 
 
 def before_feature(context, feature):
