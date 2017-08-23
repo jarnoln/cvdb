5f371e9814897990a23927a568f639f4e2f90398
Jarno Luoma-Nirva <jln@iki.fi>
2017-08-23 12:09:27 +0300

    UT:Test signing up and logging in

--- viewcv/tests/test_account_views.py
+++ viewcv/tests/test_account_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from django.contrib import auth
 
 
 class SignupTest(TestCase):
@@ -18,6 +19,19 @@ class SignupTest(TestCase):
         response = self.client.get(reverse('account_signup'))
         self.assertTemplateUsed(response, 'account/signup.html')
 
+    def test_signup(self):
+        self.assertEqual(auth.models.User.objects.count(), 0)
+        response = self.client.post(reverse('account_signup'), {
+            'username': 'user',
+            'email': 'user@iki.fi',
+            'password1': 'password',
+            'password2': 'password'}, follow=True)
+        self.assertEqual(auth.models.User.objects.count(), 1)
+        self.assertTrue(response.context['user'].is_authenticated())
+        self.assertEqual(response.context['user'], auth.models.User.objects.first())
+        html = response.content.decode('utf8')
+        self.assertInHTML('Logout', html)
+
 
 class LoginTest(TestCase):
     def test_reverse(self):
@@ -34,3 +48,35 @@ class LoginTest(TestCase):
     def test_uses_correct_template(self):
         response = self.client.get(reverse('account_login'))
         self.assertTemplateUsed(response, 'account/login.html')
+
+    def test_login(self):
+        user = auth.models.User.objects.create(username='user', email='user@iki.fi')
+        user.set_password('password')
+        user.save()
+        response = self.client.post(reverse('account_login'), {
+            'login': user.username,
+            'password': 'password',
+            'next': reverse('home')}, follow=True)
+        self.assertTrue(response.context['user'].is_authenticated())
+        self.assertEqual(response.context['user'], user)
+        html = response.content.decode('utf8')
+        self.assertInHTML('Logout', html)
+
+
+class ProfileTest(TestCase):
+    def create_and_log_in_user(self):
+        user = auth.models.User.objects.create(username='test_user', email='tuser@iki.fi')
+        user.set_password('password')
+        user.save()
+        response = self.client.post(reverse('account_login'), {'login': user.username, 'password': 'password'})
+        html = response.content.decode('utf8')
+        print(html)
+        return user
+
+    def test_reverse(self):
+        self.assertEqual(reverse('profile'), '/accounts/profile/')
+
+    def test_uses_correct_template(self):
+        user = self.create_and_log_in_user()
+        response = self.client.get(reverse('profile'))
+        self.assertTemplateUsed(response, 'viewcv/profile.html')
