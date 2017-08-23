f5f85bbac82ecda6ed0af3c25d77be721bae80f1
Jarno Luoma-Nirva <jln@iki.fi>
2017-08-23 10:00:32 +0300

    BDD: Tests for clicking menu links

--- features/cvdb.feature
+++ features/cvdb.feature
@@ -1,8 +1,22 @@
-Feature: CVDB
+Feature: Home page
     As a user
-    I want to see default Django page
+    I want to see home page titled "CVDB"
+    and links to log in and sign up
+    and if I click the links, I will see proper page.
 
-    Scenario: Open front page and check title
-
+    Scenario: Open front page, check title and links
         When I open root page
         Then I will see title "CVDB"
+         And I will see link "Login"
+         And I will see link "Sign Up"
+
+    Scenario: Sign Up
+        When I open root page
+         And I will click link "Sign Up"
+        Then I will see title "Sign Up"
+
+    Scenario: Login
+        When I open root page
+         And I will click link "Login"
+        Then I will see title "Sign In"
+
--- features/steps/my_lists.py
+++ features/steps/my_lists.py
@@ -1,3 +1,4 @@
+from time import sleep
 from behave import given, when, then
 # from django.conf import settings
 
@@ -6,8 +7,21 @@ from behave import given, when, then
 def open_root_page(context):
     # context.browser.get(context.get_url('/'))
     context.browser.get('http://127.0.0.1:8000/')
+    sleep(1)
+
+
+@when(u'I will click link "{text}"')
+def step_impl(context, text):
+    context.browser.find_element_by_link_text(text).click()
+    sleep(2)
 
 
 @then(u'I will see title "{title}"')
 def step_impl(context, title):
     context.test.assertIn(title, context.browser.title)
+
+
+@then(u'I will see link "{text}"')
+def step_impl(context, text):
+    link = context.browser.find_element_by_link_text(text)
+    context.test.assertEqual(text, link.text)
