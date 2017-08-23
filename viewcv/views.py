4d9c744dcd3cd25e30407f3c6b5b3418b448dca3
Jarno Luoma-Nirva <jln@iki.fi>
2017-08-23 12:42:32 +0300

    Profile page shows proper profile data

--- viewcv/views.py
+++ viewcv/views.py
@@ -1,9 +1,13 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
+from django.contrib import auth
 
 
 class HomeView(TemplateView):
     template_name = 'viewcv/home.html'
 
 
-class ProfileView(TemplateView):
+class ProfileView(DetailView):
     template_name = 'viewcv/profile.html'
+
+    def get_object(self, queryset=None):
+        return auth.get_user(self.request)
