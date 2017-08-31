46493210bf147de0c9af0bcdecd9a027eb257d02
Jarno Luoma-Nirva <jln@iki.fi>
2017-08-31 17:36:39 +0300

    UT:Test submitting two work entries

--- api01/tests/test_api.py
+++ api01/tests/test_api.py
@@ -16,34 +16,56 @@ class ApiRootTest(TestCase):
         # self.assertEqual(data['drive'], 'http://testserver/api/01/drive/')
 
 
-class SubmitResumeTest(TestCase):
-    def test_submit_resume(self):
-        resume = {
-            "basics": {
-                "label": "Journalist",
-                "summary": ""
-            },
-            "work": [
-                {
-                    "company": "Daily Bugle",
-                    "position": "Reporter",
-                    "startDate": "1945-01-01",
-                    "endDate": "2020-01-01",
-                    "summary": "Specialized in Superman stories",
-                }
-            ]
-        }
+def get_work_bugle():
+    work = {
+        "company": "Daily Bugle",
+        "position": "Reporter",
+        "startDate": "1945-01-01",
+        "endDate": "2020-01-01",
+        "summary": "Specialized in Superman stories",
+    }
+    return work
 
+
+def get_resume():
+    resume = {
+        "basics": {
+            "label": "Journalist",
+            "summary": ""
+        },
+        "work": []
+    }
+    return resume
+
+
+class SubmitResumeTest(TestCase):
+    def test_submit_resume_with_two_work_entries(self):
+        resume = get_resume()
+        work_1 = get_work_bugle()
+        work_2 = {
+            "company": "Jonah's farm",
+            "position": "Farmhand",
+            "startDate": "1940-01-01",
+            "endDate": "1944-12-01",
+            "summary": "Helping my parents at farm",
+        }
+        resume['work'] = [work_1, work_2]
         resume_json = json.dumps(resume)
         self.assertEqual(Work.objects.count(), 0)
         response = self.client.post('/api/01/resume/', data=resume_json, content_type='application/json')
-        self.assertEqual(Work.objects.count(), 1)
-        work_1 = Work.objects.first()
+        self.assertEqual(Work.objects.count(), 2)
+        work_1 = Work.objects.all()[0]
+        work_2 = Work.objects.all()[1]
         # print(response.content)
         data = json.loads(response.content.decode('utf8'))
         # print(data)
-        self.assertEqual(work_1.company, "Daily Bugle")
-        self.assertEqual(work_1.position, "Reporter")
-        self.assertEqual(work_1.summary, "Specialized in Superman stories")
-        self.assertEqual(work_1.start_date, datetime.date(1945, 1, 1))
-        self.assertEqual(work_1.end_date, datetime.date(2020, 1, 1))
+        self.assertEqual(work_1.company, "Jonah's farm")
+        self.assertEqual(work_1.position, "Farmhand")
+        self.assertEqual(work_1.summary, "Helping my parents at farm")
+        self.assertEqual(work_1.start_date, datetime.date(1940, 1, 1))
+        self.assertEqual(work_1.end_date, datetime.date(1944, 12, 1))
+        self.assertEqual(work_2.company, "Daily Bugle")
+        self.assertEqual(work_2.position, "Reporter")
+        self.assertEqual(work_2.summary, "Specialized in Superman stories")
+        self.assertEqual(work_2.start_date, datetime.date(1945, 1, 1))
+        self.assertEqual(work_2.end_date, datetime.date(2020, 1, 1))
--- viewcv/models.py
+++ viewcv/models.py
@@ -23,3 +23,6 @@ class Work(models.Model):
 
     def __str__(self):
         return '{}:{}'.format(self.company, self.position)
+
+    class Meta:
+        ordering = ['start_date']
