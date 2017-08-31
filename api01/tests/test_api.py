63c21ebd19eb61902ae0200c642f5959d807f118
Jarno Luoma-Nirva <jln@iki.fi>
2017-08-31 17:04:39 +0300

    API:Submit created work entries

--- api01/tests/test_api.py
+++ api01/tests/test_api.py
@@ -1,5 +1,6 @@
 import json
 from django.test import TestCase
+from viewcv.models import Cv, Work
 
 
 class ApiRootTest(TestCase):
@@ -9,7 +10,7 @@ class ApiRootTest(TestCase):
         # self.assertContains(response, 'Api Root')
         response_string = response.content.decode('utf8')
         data = json.loads(response_string)
-        print(data)
+        # print(data)
         # self.assertEqual(data['process'], 'http://testserver/api/01/process/')
         # self.assertEqual(data['drive'], 'http://testserver/api/01/drive/')
 
@@ -27,17 +28,19 @@ class SubmitResumeTest(TestCase):
                     "position": "Reporter",
                     "startDate": "1945-01-01",
                     "endDate": "2020-01-01",
+                    "summary": "Specialized in Superman stories",
                 }
             ]
         }
 
         resume_json = json.dumps(resume)
-        # self.assertEqual(Work.objects.count(), 0)
+        self.assertEqual(Work.objects.count(), 0)
         response = self.client.post('/api/01/resume/', data=resume_json, content_type='application/json')
-        # self.assertEqual(Work.objects.count(), 1)
-        # work_1 = Work.objects.first()
+        self.assertEqual(Work.objects.count(), 1)
+        work_1 = Work.objects.first()
         # print(response.content)
         data = json.loads(response.content.decode('utf8'))
-        print(data)
-        # self.assertEqual(data['id'], trace_1.id)
-        # self.assertEqual(trace_1.name, trace['name'])
+        # print(data)
+        self.assertEqual(work_1.company, "Daily Bugle")
+        self.assertEqual(work_1.position, "Reporter")
+        self.assertEqual(work_1.summary, "Specialized in Superman stories")
--- api01/views.py
+++ api01/views.py
@@ -1,12 +1,18 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
+from viewcv.models import Cv, Work
+from .serializers import WorkSerializer
 
 
 @api_view(['POST'])
 def submit_resume(request):
     """ Submit trace """
     data = JSONParser().parse(request)
+    work_list = data['work']
+    for work_item in work_list:
+        Work.objects.create(company=work_item['company'], position=work_item['position'], summary=work_item['summary'])
+
     return JsonResponse(data, status=200, safe=False)
     # serializer = TraceSerializer(data=data)
     # if serializer.is_valid():
