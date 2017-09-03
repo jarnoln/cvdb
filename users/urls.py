1c43cbc3c051be26ca603c8f2228b1cf8d7842e2
Jarno Luoma-Nirva <jln@iki.fi>
2017-09-04 00:05:01 +0300

    Code cleanup

--- users/urls.py
+++ users/urls.py
@@ -4,9 +4,9 @@ from .views import CvUserList, CvUserDetail, CvUserUpdate, CvUserDelete
 
 
 urlpatterns = [
-    url(r'^user/(?P<slug>[\w\.-]+)/edit/$', login_required(CvUserUpdate.as_view()), name='user_update'),
-    url(r'^user/(?P<slug>[\w\.-]+)/delete/$', login_required(CvUserDelete.as_view()), name='user_delete'),
-    url(r'^user/(?P<slug>[\w\.-]+)/$', CvUserDetail.as_view(), name='user_detail'),
+    url(r'^user/(?P<slug>[\w]+)/edit/$', login_required(CvUserUpdate.as_view()), name='user_update'),
+    url(r'^user/(?P<slug>[\w]+)/delete/$', login_required(CvUserDelete.as_view()), name='user_delete'),
+    url(r'^user/(?P<slug>[\w]+)/$', CvUserDetail.as_view(), name='user_detail'),
     url(r'^users/$', login_required(CvUserList.as_view()), name='user_list'),
     url(r'^accounts/profile/$', login_required(CvUserDetail.as_view()), name='profile'),
 ]
