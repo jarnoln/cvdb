308f34062e1008d0125f1ec2d1f24a016b0e821f
Jarno Luoma-Nirva <jln@iki.fi>
2017-09-04 01:00:56 +0300

    Caclulate work durations

--- viewcv/models.py
+++ viewcv/models.py
@@ -29,6 +29,43 @@ class Work(models.Model):
     end_date = models.DateField(null=True, blank=True)
     summary = models.TextField(blank=True, default='')
 
+    @property
+    def duration(self):
+        years = self.end_date.year - self.start_date.year
+        months = self.end_date.month - self.start_date.month
+        if months < 0:
+            years = years - 1
+            months = months + 12
+        return years, months
+
+    @property
+    def duration_years(self):
+        years, months = self.duration
+        return years
+
+    @property
+    def duration_months(self):
+        years, months = self.duration
+        return months
+
+    @property
+    def duration_str(self):
+        years, months = self.duration
+        duration_y = ''
+        duration_m = ''
+        if years > 1:
+            duration_y = '{} years'.format(years)
+        elif years == 1:
+            duration_y = '1 year'
+        if months > 1:
+            duration_m = '{} months'.format(months)
+        elif months == 1:
+            duration_m = '1 month'
+        if duration_y and duration_m:
+            return '{}, {}'.format(duration_y, duration_m)
+
+        return duration_y + duration_m
+
     def __str__(self):
         return '{}:{}'.format(self.company, self.position)
 
--- viewcv/tests/test_models.py
+++ viewcv/tests/test_models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.core.urlresolvers import reverse
 from django.contrib import auth
 from django.test import TestCase
@@ -44,3 +45,39 @@ class WorkModelTest(TestCase):
         cv = Cv.objects.create(user=user, name='cv', title='CV')
         work = Work.objects.create(cv=cv, company='Daily Bugle', position='Reporter')
         self.assertEqual(str(work), '{}:{}'.format(work.company, work.position))
+
+    def test_duration(self):
+        user = auth.get_user_model().objects.create(username='creator')
+        cv = Cv.objects.create(user=user, name='cv', title='CV')
+        work = Work.objects.create(cv=cv, company='Daily Bugle', position='Reporter',
+                                   start_date=datetime.date(2000, 1, 1),
+                                   end_date=datetime.date(2000, 2, 1))
+        self.assertEqual(work.duration_years, 0)
+        self.assertEqual(work.duration_months, 1)
+        self.assertEqual(work.duration_str, '1 month')
+        work = Work.objects.create(cv=cv, company='Daily Bugle', position='Reporter',
+                                   start_date=datetime.date(2000, 1, 1),
+                                   end_date=datetime.date(2001, 1, 1))
+        self.assertEqual(work.duration_years, 1)
+        self.assertEqual(work.duration_months, 0)
+        self.assertEqual(work.duration_str, '1 year')
+        work = Work.objects.create(cv=cv, company='Daily Bugle', position='Reporter',
+                                   start_date=datetime.date(2000, 1, 1),
+                                   end_date=datetime.date(2001, 2, 1))
+        self.assertEqual(work.duration_years, 1)
+        self.assertEqual(work.duration_months, 1)
+        self.assertEqual(work.duration_str, '1 year, 1 month')
+
+        work = Work.objects.create(cv=cv, company='Daily Bugle', position='Reporter',
+                                   start_date=datetime.date(2000, 1, 1),
+                                   end_date=datetime.date(2002, 3, 1))
+        self.assertEqual(work.duration_years, 2)
+        self.assertEqual(work.duration_months, 2)
+        self.assertEqual(work.duration_str, '2 years, 2 months')
+
+        work = Work.objects.create(cv=cv, company='Daily Bugle', position='Reporter',
+                                   start_date=datetime.date(2000, 6, 1),
+                                   end_date=datetime.date(2002, 3, 1))
+        self.assertEqual(work.duration_years, 1)
+        self.assertEqual(work.duration_months, 9)
+        self.assertEqual(work.duration_str, '1 year, 9 months')
